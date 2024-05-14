b65d736b517c986259161a6b4df316dcb237ff81
Nguyễn Huỳnh Duy Tân <92073232+nixOMG@users.noreply.github.com>
2024-05-14 21:46:56 +0700

    Fix edit user info

--- NewsProject/src/main/java/controller/UserController.java
+++ NewsProject/src/main/java/controller/UserController.java
@@ -6,6 +6,7 @@ import java.sql.Date;
 import javax.persistence.EntityManager;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -18,6 +19,7 @@ import utils.DBUtil;
 
 
 @WebServlet({ "/UserController", "/manage-user-info" })
+@MultipartConfig
 public class UserController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
